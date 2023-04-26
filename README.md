48567815abf44e42b51c5fcc588d0f56e3ef4e8c
lemanh-tuong <55147271+lemanh-tuong@users.noreply.github.com>
2023-04-26 08:36:08 +0000

    Codesandbox => Update: update style + fix lỗi README.md không hiển thị ảnh trên codesandbox editor

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Người chịu trách nhiệm: Đoàn Văn
 Người nghiệm thu: Nguyen Dttn
 Status: Not started
 
-![Screenshot 2023-04-26 at 10.32.36.png](docs/Screenshot_2023-04-26_at_10.32.36.png)
+![Screenshot 2023-04-26 at 10.32.36.png](././docs/Screenshot_2023-04-26_at_10.32.36.png)
 
 1. Đổi tên từ Deadline → Countdown Timer @Đoàn Văn 
 2. Cần thêm description cho field này:
     
     Click here to convert the ended date to timestamp @Đoàn Văn Link sẽ dẫn đến 1 trang để khách có thể chọn thời gian (dạng human readable ) và tự động convert giá trị đó sang timestamp. 
     
-    ![Screenshot 2023-04-26 at 10.40.36.png](docs/Screenshot_2023-04-26_at_10.40.36.png)
+    ![Screenshot 2023-04-26 at 10.40.36.png](././docs/Screenshot_2023-04-26_at_10.40.36.png)
     
     Cần làm 1 trang convert ở Codesandbox để người dùng chọn thời gian và sau đó copy timestamp @Tưởng Lê Mạnh
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { Card, Col, DatePicker, DatePickerProps, Descriptions, Form, Input, Row, Space, Typography } from 'antd';
-import Countdown from 'antd/es/statistic/Countdown';
-import dayjs, { Dayjs } from 'dayjs';
-import { useEffect, useState } from 'react';
-import { CopyButton } from './components/CopyButton/CopyButton';
-import { DateTime } from './components/DateTime';
-import './styles.css';
+import {
+  Card,
+  Col,
+  DatePicker,
+  DatePickerProps,
+  Descriptions,
+  Form,
+  Input,
+  Row,
+  Space,
+  Typography,
+} from "antd";
+import Countdown from "antd/es/statistic/Countdown";
+import dayjs, { Dayjs } from "dayjs";
+import { useEffect, useState } from "react";
+import { CopyButton } from "./components/CopyButton/CopyButton";
+import { DateTime } from "./components/DateTime";
+import "./styles.css";
 
-const FORMAT = 'DD/MM/YYYY HH:mm:ss';
+const FORMAT = "DD/MM/YYYY HH:mm:ss";
 
 function App() {
-  const [state, setState] = useState<Dayjs>(dayjs().add(5, 'minutes'));
+  const [state, setState] = useState<Dayjs>(dayjs().add(5, "minutes"));
   const [now, setNow] = useState(dayjs());
 
-  const handleChange: DatePickerProps['onChange'] = value => {
+  const handleChange: DatePickerProps["onChange"] = (value) => {
     if (value) {
       setState(value);
     }
@@ -33,14 +44,22 @@ function App() {
               <Form.Item
                 label="Select a Date and Time"
                 tooltip="Use the date and time picker to select the date and time that you want to count down to."
+                extra="Format is DD/MM/YYYY HH:mm:ss"
               >
-                <DatePicker showTime allowClear={false} format={FORMAT} value={state} onChange={handleChange} />
+                <DatePicker
+                  showTime
+                  allowClear={false}
+                  format={FORMAT}
+                  value={state}
+                  onChange={handleChange}
+                />
               </Form.Item>
             </Col>
             <Col sm={12} xs={24}>
               <Form.Item
-                label="Countdown Timestamp (include miliseconds)"
+                label="Countdown Timestamp"
                 tooltip="The timestamp value of the countdown date and time."
+                extra="Timestamp is include miliseconds"
               >
                 <Space.Compact>
                   <Input readOnly value={state.valueOf()} />
@@ -51,8 +70,12 @@ function App() {
           </Row>
 
           <Descriptions bordered column={1}>
-            <Descriptions.Item label="Now">{now.format(FORMAT)}</Descriptions.Item>
-            <Descriptions.Item label="Time end">{state.format(FORMAT)}</Descriptions.Item>
+            <Descriptions.Item label="Now">
+              {now.format(FORMAT)}
+            </Descriptions.Item>
+            <Descriptions.Item label="Time end">
+              {state.format(FORMAT)}
+            </Descriptions.Item>
             <Descriptions.Item
               label={
                 <Typography.Text type="danger" strong>
@@ -60,7 +83,8 @@ function App() {
                 </Typography.Text>
               }
             >
-              <Typography.Text>End after:</Typography.Text> <Countdown value={state.valueOf()} />
+              <Typography.Text>End after:</Typography.Text>{" "}
+              <Countdown value={state.valueOf()} />
             </Descriptions.Item>
           </Descriptions>
           <DateTime value={now} />
